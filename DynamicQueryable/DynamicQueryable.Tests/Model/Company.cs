a51f9257b301b0fb031cd82bfa3476031a4a4bb1
Umut Özel <umutozel@gmail.com>
2017-06-14 12:06:59 +0300

    Adding test models

---
+++ DynamicQueryable/DynamicQueryable.Tests/Model/Company.cs
@@ -0,0 +1,8 @@
+﻿namespace DynamicQueryable.Tests.Model {
+
+    public class Company {
+        public int Id { get; set; }
+        public string CompanyName { get; set; }
+        public string Phone { get; set; }
+    }
+}
