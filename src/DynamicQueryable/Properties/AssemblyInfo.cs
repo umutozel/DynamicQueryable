4b97e3f7f004c35b518db3a95d74c90f8b31daac
Umut Özel <umutozel@outlook.com>
2018-09-27 09:47:48 +0300

    add AssemblyInfo

---
+++ src/DynamicQueryable/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DynamicQueryable - Construct Linq queries using strings.")]
+[assembly: AssemblyDescription("Construct Linq queries using strings.")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+[assembly: AssemblyCompany("Umut Özel")]
+[assembly: AssemblyProduct("DynamicQueryable")]
+[assembly: AssemblyCopyright("Copyright © 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
